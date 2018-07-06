96fcedee8206700efd59b88a75d20b48939dba15
Kryptolyser <yannik.dittmar@gmx.de>
2018-07-06 23:05:23 +0200

    update logger started

---
+++ UpdateLogger/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace UpdateLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(609, 333);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 3);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(603, 288);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(205, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Log Changes";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(3, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(408, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 333);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UpdateLogger/Form1.cs
@@ -0,0 +1,139 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateLogger
+{
+    public partial class Form1 : Form
+    {
+        private string settingsFile = @"";
+        private string scanLocation = @"D:\MegaSync\Visual-Studio\Script-Browser_SLCB\Script-Browser\bin\Debug";
+        private JArray Changelog; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                if (File.Exists(settingsFile))
+                    Changelog = JArray.Parse(File.ReadAllText(settingsFile));
+                else
+                    Changelog = new JArray();
+            }
+            catch { Changelog = new JArray(); }
+
+            JObject lastScan = new JObject();
+            lastScan["files"] = new JArray();
+            lastScan["folders"] = new JArray();
+
+            textBox1.Text = ScanChanges(lastScan).ToString().Replace("\\\\", "\\");
+            textBox1.Select(0, 0);
+        }
+
+        private JObject ScanChanges(JObject lastScan)
+        {
+            JObject result = new JObject
+            {
+                ["date"] = DateTime.Now.ToString("dd.MM.yy"),
+                ["files"] = new JObject
+                {
+                    ["stats"] = new JObject
+                    {
+                        ["changed"] = 0,
+                        ["removed"] = 0
+                    },
+                    ["changed"] = new JArray(),
+                    ["removed"] = new JArray()
+                },
+                ["folders"] = new JObject
+                {
+                    ["stats"] = new JObject
+                    {
+                        ["changed"] = 0,
+                        ["removed"] = 0
+                    },
+                    ["changed"] = new JArray(),
+                    ["removed"] = new JArray()
+                }
+            };
+
+            string[][] subData = GetSubData(scanLocation);
+            string[] currentFiles = subData[0];
+            string[] currentFolders = subData[1];
+
+            //Removed Files
+            foreach (JToken lastfile in lastScan["files"])
+            {
+                if (!currentFiles.Contains(lastfile.ToString()))
+                    ((JArray)result["files"]["removed"]).Add(lastfile);
+            }
+
+            //Removed Folders
+            foreach (JToken lastfolder in lastScan["folders"])
+            {
+                if (!currentFolders.Contains(lastfolder.ToString()))
+                    ((JArray)result["folders"]["removed"]).Add(lastfolder);
+            }
+
+            //Added Files
+            foreach (string currentFile in currentFiles)
+            {
+                if (!((JArray)lastScan["files"]).Contains(currentFile))
+                    ((JArray)result["files"]["changed"]).Add(currentFile);
+            }
+
+            //Added Folders
+            foreach (string currentFolder in currentFolders)
+            {
+                if (!((JArray)lastScan["folders"]).Contains(currentFolder))
+                    ((JArray)result["folders"]["changed"]).Add(currentFolder);
+            }
+
+            //Counters
+            result["files"]["stats"]["changed"] = result["files"]["changed"].Count();
+            result["files"]["stats"]["removed"] = result["files"]["removed"].Count();
+            result["folders"]["stats"]["changed"] = result["folders"]["changed"].Count();
+            result["folders"]["stats"]["removed"] = result["folders"]["removed"].Count();
+
+            return result;
+        }
+
+        private string[][] GetSubData(string p)
+        {
+            HashSet<string> files = new HashSet<string>();
+            HashSet<string> subfolders = new HashSet<string>(Directory.GetDirectories(p));
+            HashSet<string> storedSubfolders = new HashSet<string>();
+
+            string[] topFiles = Directory.GetFiles(p);
+            foreach (string file in topFiles)
+                files.Add(file.Replace(p, ""));
+
+            while (subfolders.Count != 0)
+            {
+                string subfolder = subfolders.First();
+                string[] subfiles = Directory.GetFiles(subfolder);
+
+                foreach (string file in subfiles)
+                    files.Add(file.Replace(p, ""));
+
+                subfolders.Remove(subfolder);
+                storedSubfolders.Add(subfolder.Replace(p, ""));
+
+                string[] subsubfolders = Directory.GetDirectories(subfolder);
+                foreach (string subsubfolder in subsubfolders)
+                    subfolders.Add(subsubfolder);
+            }
+
+            return new string[][] { files.ToArray(), storedSubfolders.ToArray()};
+        }
+    }
+}
